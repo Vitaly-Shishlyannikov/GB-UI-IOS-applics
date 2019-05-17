85ab5c2b4d2bf3face1751bb0f79f2146fe45c06
Vit_sh <vithits@yandex.ru>
2019-05-18 00:57:32 +0300

    Создана пустая форма для входа

---
+++ L1_Vitaly_Shishlyannikov/GB-Vkontakte/GB-Vkontakte/LaunchController.swift
@@ -0,0 +1,32 @@
+//
+//  LaunchController.swift
+//  GB-Vkontakte
+//
+//  Created by Vitalik on 17.05.2019.
+//  Copyright © 2019 Vit. All rights reserved.
+//
+
+import UIKit
+
+class LaunchController: UIViewController {
+   
+    @IBOutlet weak var labelVK: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
