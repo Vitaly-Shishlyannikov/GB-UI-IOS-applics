77e9c97f03c035ae937bf61485b3dd1231fc7ff3
Vit_sh <vithits@yandex.ru>
2019-06-18 12:46:26 +0300

    исправлены ошибки с индексами крайнних фотогорафий, но осталась проблема задвоения при перелистывании карайних фотографий

--- L1_Vitaly_Shishlyannikov/GB-Vkontakte/GB-Vkontakte/Controllers/FullScreeenPhotoController.swift
+++ L1_Vitaly_Shishlyannikov/GB-Vkontakte/GB-Vkontakte/Controllers/FullScreeenPhotoController.swift
@@ -22,6 +22,7 @@ class FullScreenPhotoController: UIViewController  {
         getPhotos()
         setPhotos()
         mainPhoto.addGestureRecognizer(swipeLeftGestureRecognizer)
+        mainPhoto.addGestureRecognizer(swipeRightGestureRecognizer)
     }
     
     func getPhotos () {
@@ -29,60 +30,101 @@ class FullScreenPhotoController: UIViewController  {
     }
     
     func setPhotos () {
-        let currentPhoto = photos[indexPathSelected.row]
-        let photoPath = currentPhoto.photoPath
+        if indexPathSelected.row >= 0,
+            indexPathSelected.row < photos.count - 1 {
+        let currentPhotoIndex = photos[indexPathSelected.row]
+        let photoPath = currentPhotoIndex.photoPath
         mainPhoto.image = UIImage(named: photoPath)
         
-        let nextPhotoModel = photos[indexPathSelected.row + 1]
-        let nextPhotoPath = nextPhotoModel.photoPath
+        let nextPhotoIndex = photos[indexPathSelected.row + 1]
+        let nextPhotoPath = nextPhotoIndex.photoPath
         nextPhoto.image = UIImage(named: nextPhotoPath)
+        }
     }
     
     lazy var swipeLeftGestureRecognizer: UISwipeGestureRecognizer = {
         let recognizerLeft = UISwipeGestureRecognizer(target: self,
-                                                action: #selector(onSwipe(_:)))
+                                                action: #selector(onSwipeLeft(_:)))
         recognizerLeft.direction = .left
         return recognizerLeft
-        
     }()
     
-    @objc func onSwipe(_ sender: UITapGestureRecognizer) {
-        animatedPhotoRemoving()
-     
+    @objc func onSwipeLeft(_ sender: UITapGestureRecognizer) {
+        if indexPathSelected.row < photos.count - 1 {
+        animatedPhotoChangeLeft()
+        indexPathSelected.row += 1
+        //setPhotos()
+        }
     }
     
-    func animatedPhotoRemoving() {
+    func animatedPhotoChangeLeft() {
         
         let animationScale = CABasicAnimation(keyPath: "transform.scale")
         animationScale.fromValue = 1
         animationScale.toValue = 0
-        animationScale.duration = 2
+        animationScale.duration = 1
         animationScale.beginTime = CACurrentMediaTime()
         animationScale.fillMode = CAMediaTimingFillMode.backwards
         
         let animationOpacity = CABasicAnimation(keyPath: "opacity")
         animationOpacity.toValue = 0
-        animationOpacity.duration = 2
+        animationOpacity.duration = 1
         animationOpacity.fillMode = CAMediaTimingFillMode.backwards
         
         self.mainPhoto.layer.add(animationScale, forKey: nil)
         self.mainPhoto.layer.add(animationOpacity, forKey: nil)
         
-        let animation2 = CABasicAnimation(keyPath: "position.x")
-        animation2.fromValue = 500
-        animation2.toValue = UIScreen.main.bounds.width / 2
-        animation2.duration = 2
-        animation2.beginTime = CACurrentMediaTime()
+        let animationMoveLeft = CABasicAnimation(keyPath: "position.x")
+        animationMoveLeft.fromValue = 500
+        animationMoveLeft.toValue = UIScreen.main.bounds.width / 2
+        animationMoveLeft.duration = 1
+        animationMoveLeft.beginTime = CACurrentMediaTime()
         
-        self.nextPhoto.layer.add(animation2, forKey: nil)
+        self.nextPhoto.layer.add(animationMoveLeft, forKey: nil)
         
-        //indexPathSelected.row += 1
-        let a = indexPathSelected.row
-        //setPhotos()
+        setPhotos()
+    }
+    
+    lazy var swipeRightGestureRecognizer: UISwipeGestureRecognizer = {
+        let recognizerRight = UISwipeGestureRecognizer(target: self,
+                                                       action: #selector(onSwipeRight(_:)))
+        recognizerRight.direction = .right
+        return recognizerRight
+    }()
+    
+    @objc func onSwipeRight(_ sender: UITapGestureRecognizer) {
+        if indexPathSelected.row >= 0 {
+            animatedPhotoChangeRight()
+            indexPathSelected.row -= 1
+            //setPhotos()
+        }
+    }
+    
+    func animatedPhotoChangeRight() {
         
-//        let nextPhotoModel = photos[indexPathSelected.row + 1]
-//        let nextPhotoPath = nextPhotoModel.photoPath
-//        largePhoto.image = UIImage(named: nextPhotoPath)
-//        indexPathSelected.row += 1
+        let animationScale = CABasicAnimation(keyPath: "transform.scale")
+        animationScale.fromValue = 1
+        animationScale.toValue = 0
+        animationScale.duration = 1
+        animationScale.beginTime = CACurrentMediaTime()
+        animationScale.fillMode = CAMediaTimingFillMode.backwards
+        
+        let animationOpacity = CABasicAnimation(keyPath: "opacity")
+        animationOpacity.toValue = 0
+        animationOpacity.duration = 1
+        animationOpacity.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.nextPhoto.layer.add(animationScale, forKey: nil)
+        self.nextPhoto.layer.add(animationOpacity, forKey: nil)
+        
+        let animationMoveLeft = CABasicAnimation(keyPath: "position.x")
+        animationMoveLeft.fromValue = -500
+        animationMoveLeft.toValue = UIScreen.main.bounds.width / 2
+        animationMoveLeft.duration = 1
+        animationMoveLeft.beginTime = CACurrentMediaTime()
+        
+        self.mainPhoto.layer.add(animationMoveLeft, forKey: nil)
+        
+        setPhotos()
     }
 }
