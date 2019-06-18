56d60b45ae8964b8b26021d7e501284ad8ec6d98
Vit_sh <vithits@yandex.ru>
2019-06-18 11:16:35 +0300

    добавлены анимации смены фотографий

--- L1_Vitaly_Shishlyannikov/GB-Vkontakte/GB-Vkontakte/Controllers/FullScreeenPhotoController.swift
+++ L1_Vitaly_Shishlyannikov/GB-Vkontakte/GB-Vkontakte/Controllers/FullScreeenPhotoController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class FullScreenPhotoController: UIViewController  {
     
-    @IBOutlet weak var largePhoto: UIImageView!
-    weak var nextPhoto: UIImageView!
+    @IBOutlet weak var mainPhoto: UIImageView!
+    @IBOutlet weak var nextPhoto: UIImageView!
     weak var previousPhoto: UIImageView!
     
     var indexPathSelected = IndexPath()
@@ -21,7 +21,7 @@ class FullScreenPhotoController: UIViewController  {
     override func viewDidLoad() {
         getPhotos()
         setPhotos()
-        largePhoto.addGestureRecognizer(swipeLeftGestureRecognizer)
+        mainPhoto.addGestureRecognizer(swipeLeftGestureRecognizer)
     }
     
     func getPhotos () {
@@ -31,12 +31,11 @@ class FullScreenPhotoController: UIViewController  {
     func setPhotos () {
         let currentPhoto = photos[indexPathSelected.row]
         let photoPath = currentPhoto.photoPath
-        largePhoto.image = UIImage(named: photoPath)
+        mainPhoto.image = UIImage(named: photoPath)
         
-//        let nextPhotoModel = photos[indexPathSelected.row + 1]
-//        let nextPhotoPath = nextPhotoModel.photoPath
-//        nextPhoto.image = UIImage(named: "photo2")
-        
+        let nextPhotoModel = photos[indexPathSelected.row + 1]
+        let nextPhotoPath = nextPhotoModel.photoPath
+        nextPhoto.image = UIImage(named: nextPhotoPath)
     }
     
     lazy var swipeLeftGestureRecognizer: UISwipeGestureRecognizer = {
@@ -49,22 +48,41 @@ class FullScreenPhotoController: UIViewController  {
     
     @objc func onSwipe(_ sender: UITapGestureRecognizer) {
         animatedPhotoRemoving()
-        let nextPhotoModel = photos[indexPathSelected.row + 1]
-        let nextPhotoPath = nextPhotoModel.photoPath
-        largePhoto.image = UIImage(named: nextPhotoPath)
-        indexPathSelected.row += 1
+     
     }
     
     func animatedPhotoRemoving() {
-        let animation = CASpringAnimation(keyPath: "transform.scale")
-        animation.fromValue = 1
-        animation.toValue = 1
-        animation.stiffness = 200
-        animation.mass = 1
-        animation.duration = 1
-        animation.beginTime = CACurrentMediaTime()
-        animation.fillMode = CAMediaTimingFillMode.backwards
         
-        self.largePhoto.layer.add(animation, forKey: nil)
+        let animationScale = CABasicAnimation(keyPath: "transform.scale")
+        animationScale.fromValue = 1
+        animationScale.toValue = 0
+        animationScale.duration = 2
+        animationScale.beginTime = CACurrentMediaTime()
+        animationScale.fillMode = CAMediaTimingFillMode.backwards
+        
+        let animationOpacity = CABasicAnimation(keyPath: "opacity")
+        animationOpacity.toValue = 0
+        animationOpacity.duration = 2
+        animationOpacity.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.mainPhoto.layer.add(animationScale, forKey: nil)
+        self.mainPhoto.layer.add(animationOpacity, forKey: nil)
+        
+        let animation2 = CABasicAnimation(keyPath: "position.x")
+        animation2.fromValue = 500
+        animation2.toValue = UIScreen.main.bounds.width / 2
+        animation2.duration = 2
+        animation2.beginTime = CACurrentMediaTime()
+        
+        self.nextPhoto.layer.add(animation2, forKey: nil)
+        
+        //indexPathSelected.row += 1
+        let a = indexPathSelected.row
+        //setPhotos()
+        
+//        let nextPhotoModel = photos[indexPathSelected.row + 1]
+//        let nextPhotoPath = nextPhotoModel.photoPath
+//        largePhoto.image = UIImage(named: nextPhotoPath)
+//        indexPathSelected.row += 1
     }
 }
